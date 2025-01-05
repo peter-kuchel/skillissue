5dd5a3f827734baa1ef827f725be1ca2506d3820
Peter Kuchel <petermichaelkuchel@gmail.com>
2025-01-04 17:54:10 -0700

    wrote 2 tests to check position of cursor

--- tests/screen_tests.c
+++ tests/screen_tests.c
@@ -1,9 +1,5 @@
 #include "screen_tests.h"
 
-int test_scrolling_down(){
-     return 0;
-}
-
 void setup_test_objects(piece_table *pt, FILE* f, termw_info *tinfo, line_view *lv){
 
      if ( init_piece_table(f, TEST_FILE, pt) < 0 ){
@@ -16,14 +12,37 @@ void setup_test_objects(piece_table *pt, FILE* f, termw_info *tinfo, line_view *
      
      log_piece_table_current(&sk_logger, pt); 
 }
-int screen_test_scrolling(piece_table *pt, line_view *lv, cursor_pos *pos){
+
+int screen_test_moving_across_and_down_to_first_line(piece_table *pt, line_view *lv, cursor_pos *pos){
+     int i;
+
+     // move to the very endd of the 1st line and then it should automatically jump down to the next
+     for (i = 0; i < 79; i++) sim_pressing_D(pt, pos, lv);
+
+     // move up one space back to the beginning of the file
+     for (i = 0; i < 1; i++) sim_pressing_W(pt, pos, lv);
+
+     return pos->x == 0 && pt->curr_chr_ptr == 0;
+}
+
+int screen_test_scrolling_standard(piece_table *pt, line_view *lv, cursor_pos *pos){
      
      int i;
 
      // move the cursor 8 places to the right
      for (i = 0; i < 8; i++) sim_pressing_D(pt, pos, lv);
+     
+     // move the cursor down to the bottom of the document 
+     for (i = 0; i < 15; i++) sim_pressing_S(pt, pos, lv);
 
-     return 0;
+     // move the cursor back up to the top 
+     for (i = 0; i < 15; i++) sim_pressing_W(pt, pos, lv);
+
+     // move the cursor back to the beginning of the document
+     for (i = 0; i < 8; i++) sim_pressing_A(pt, pos, lv);
+     
+     // assert that chr position and x pos are both 0
+     return pos->x == 0 && pt->curr_chr_ptr == 0;
 }
 int main(){
 
@@ -32,14 +51,30 @@ int main(){
      cursor_pos pos = { 0, 0 };
      termw_info tinfo = { 8, 76 };
      
-     printf("[SCREEN TESTS STARTING]\n");
+     printf("\e[0;32m--- [SCREEN TESTS STARTING] ---\e[0m\n");
      FILE* f = fopen(TEST_FILE, "r+");
 
      setup_logger(&sk_logger, LOG_FILE);
      setup_test_objects(&pt, f, &tinfo, &lv);
-     screen_test_scrolling(&pt, &lv, &pos);
-		     
      fclose(f); 
-     printf("[SCREEN TESTS FINISHED]\n");
+     
+     int res;
+     res = screen_test_moving_across_and_down_to_first_line(&pt, &lv, &pos);
+     if (res == 0){
+          printf("\e[0;31m[TEST FAILED -- screen_test_moving_across_and_down_to_first_line]\e[0m\n");
+	  printf("Expected results :: curr_chr_ptr = %d, x = %d\n", 0, 0);
+	  printf("Actual results :: curr_chr_ptr = %ld, x = %d\n", pt.curr_chr_ptr, pos.x);
+	  exit(1);
+     }
+
+     res = screen_test_scrolling_standard(&pt, &lv, &pos);
+     if (res == 0){
+          printf("\e[0;31m[TEST FAILED -- screen_test_scrolling_standard]\e[0m\n");
+	  printf("Expected results :: curr_chr_ptr = %d, x = %d\n", 0, 0);
+	  printf("Actual results :: curr_chr_ptr = %ld, x = %d\n", pt.curr_chr_ptr, pos.x);
+	  exit(1);
+     }
+		     
+     printf("--- [SCREEN TESTS FINISHED] ---\n");
      return 0;
 }
